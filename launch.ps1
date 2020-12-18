675d63edf88dcfb720b192e068b3e3be60887957
James DLD <james.dumont@insight.com>
2020-12-18 10:20:19 +0100

    Dogin everything through PowerShell

--- launch.ps1
+++ launch.ps1
@@ -26,31 +26,65 @@ foreach ($item in Get-Item .\initiatives\*\policy.parameters.json) {
   $parameters = (Get-Content -Path $item.FullName) | ConvertFrom-Json
   
   Write-Output "Deploying the Azure Policy Initiative : $($policy.properties.displayName)"
-  New-AzPolicySetDefinition -Name $policy.name `
+  $initiativePolicy = New-AzPolicySetDefinition -Name $policy.name `
     -DisplayName $policy.properties.displayName `
     -PolicyDefinition ($policy.properties.policyDefinitions | ConvertTo-Json -Depth 5) `
     -Parameter ($parameters | ConvertTo-Json) `
     -Metadata  ($policy.properties.metadata | ConvertTo-Json)
+
+
+  foreach ($item_assign in Get-Item "$($item.Directory.FullName)\assign.*.json") {
+    $assign = (Get-Content -Path $item_assign.FullName) | ConvertFrom-Json
+
+    ## Assign the Initiative Policy
+    $PolicyAssignment = New-AzPolicyAssignment -Name $assign.name `
+      -DisplayName $assign.properties.displayName `
+      -PolicySetDefinition $initiativePolicy `
+      -Scope $assign.properties.scope `
+      -AssignIdentity `
+      -Location $assign.location `
+      -PolicyParameter ($assign.properties.parameters | ConvertTo-Json)
+
+    ## Extract the ObjectID of the Policy Assignment Managed Identity
+    $objectID = [GUID]($PolicyAssignment.Identity.principalId) #[GUID]($assign.identity.principalId) #
+
+    ## Create a role assignment from the role definition ids available in each policies initiatives
+    $roleDefinitionIds = $initiativePolicy.Properties.PolicyDefinitions.policyDefinitionId | ForEach-Object { (Get-AzPolicyDefinition -Id $_).Properties.policyRule.then.details.roleDefinitionIds } | Select-Object -Unique
+    foreach ($roleDefinitionId in $roleDefinitionIds) {
+      
+      New-AzRoleAssignment -Scope "$($assign.properties.scope)" -ObjectId $objectID -RoleDefinitionId "$($roleDefinitionId.Split("/")[-1])"
+
+      ## Debug because I have locally the error described here : https://stackoverflow.com/questions/63598486/azure-new-azroleassignment-input-string-was-not-in-a-correct-format-error-wit
+      Write-Host "New-AzRoleAssignment -Scope $($assign.properties.scope) -ObjectId $objectID -RoleDefinitionId $($roleDefinitionId.Split("/")[-1])"
+    }
+
+  }
+
 }
 
-#Method 2 : with GitHub Action
-<#
-1. Use Azure GiHub Action with azure/manage-azure-policy@v0, see file ./.github/workflows/manage-azure-policy.yml
- - Sample to create or update all policies : 
-    - name: Create or Update Azure Policies
-      uses: azure/manage-azure-policy@v0
-      with:
-        paths: |
-          policies/**
-          initiatives/**
-#>
+# $Test = Get-AzPolicyAssignment -Scope "/subscriptions/6094e15e-3e04-47b5-9b3b-aa8ae3cf1e52/resourceGroups/dld-corp-mvp-dataplatform"
+# $Test = $Test[1]
+# $Test.name
+# $Test.Properties.DisplayName
+# $Test.Properties.Scope
+# #Method 2 : with GitHub Action
+# <#
+# 1. Use Azure GiHub Action with azure/manage-azure-policy@v0, see file ./.github/workflows/manage-azure-policy.yml
+#  - Sample to create or update all policies : 
+#     - name: Create or Update Azure Policies
+#       uses: azure/manage-azure-policy@v0
+#       with:
+#         paths: |
+#           policies/**
+#           initiatives/**
+# #>
   
-## Policy Assignment
-## Variable
-# $scope = Get-AzResourceGroup -Name "dld-corp-mvp-dataplatform" #Replace it with your target scope
-# $logAnalytics = Get-AzOperationalInsightsWorkspace -Name "mvp-hub-logaw1" -ResourceGroupName "infr-hub-prd-rg1" #Replace it with your target Log Analytics Workspace
-# $initiativePolicy = Get-AzPolicySetDefinition -Name 'Windows Virtual Desktop Resources Diagnostic Settings' 
-# $params = @{'logAnalytics' = ($logAnalytics.ResourceId) }
+# # Policy Assignment
+# # # Variable
+# # $scope = Get-AzResourceGroup -Name "dld-corp-mvp-dataplatform" #Replace it with your target scope
+# # $logAnalytics = Get-AzOperationalInsightsWorkspace -Name "mvp-hub-logaw1" -ResourceGroupName "infr-hub-prd-rg1" #Replace it with your target Log Analytics Workspace
+# # $initiativePolicy = Get-AzPolicySetDefinition -Name 'Windows Virtual Desktop Resources Diagnostic Settings' 
+# # $params = @{'logAnalytics' = ($logAnalytics.ResourceId) }
 
 # ## Assign the Initiative Policy
 # New-AzPolicyAssignment -Name 'WVD to Log Analytics Demo' `
