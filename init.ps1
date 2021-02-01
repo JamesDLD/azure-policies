8447e7a6ab3d2ddefacbe272be6bac43e26bbf58
James DLD <james.dumont@insight.com>
2021-02-01 17:40:31 +0100

    Wording review, init script

---
+++ init.ps1
@@ -0,0 +1,34 @@
+
+<#
+This script prepare the polcicy to fit with your environment.
+#>
+
+$OldCompanyName = "MVP"
+$NewCompanyName = "DDL"
+$OldManagementGroupId = "/providers/Microsoft.Management/managementGroups/core"
+$NewManagementGroupId = "/providers/Microsoft.Management/managementGroups/xxxxxxxxxxx"
+
+#Rename multiple files 
+$items = Get-ChildItem -Path *.json -Recurse -File 
+foreach ($item in $items) {
+    $content = Get-Content -Path $item.FullName
+    if ($content -like "*$OldCompanyName*") {
+        Write-Host "Modifying file : $($item.FullName)"
+        $content -replace $OldCompanyName, $NewCompanyName > $item.FullName
+    }
+
+    if ($content -like "*$OldManagementGroupId*") {
+        Write-Host "Modifying file : $($item.FullName)"
+        $content -replace $OldManagementGroupId, $NewManagementGroupId > $item.FullName
+    }
+    
+}
+
+#Rename multiple folders
+$items = Get-ChildItem -Path ./ -Recurse -Directory
+foreach ($item in $items) {
+    if ($item.Name -like "*$OldCompanyName*") {
+        Write-Host "Renaming folder : $($item.FullName)"
+        Move-Item -Path $item.FullName $item.FullName.Replace($OldCompanyName, $NewCompanyName)
+    }
+}
